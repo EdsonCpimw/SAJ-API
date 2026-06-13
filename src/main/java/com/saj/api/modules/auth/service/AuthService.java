9e10f0d16b5623b0a37a529a0ccfc46c3fc8254b
Edson Luiz <edsoncpimw@gmail.com>
2026-06-13 18:23:12 -0300

    Recuperar usuário logado para utilizar em outros métodos

--- src/main/java/com/saj/api/modules/auth/service/AuthService.java
+++ src/main/java/com/saj/api/modules/auth/service/AuthService.java
@@ -1,6 +1,9 @@
 package com.saj.api.modules.auth.service;
 
 import com.saj.api.modules.auth.controller.dtos.AuthenticatedUser;
+import com.saj.api.modules.users.domain.entities.Company;
+import com.saj.api.modules.users.domain.entities.User;
+import com.saj.api.modules.users.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +18,9 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private static final Logger log = LoggerFactory.getLogger(AuthService.class);
 
-    public AuthenticatedUser getAuthenticatedUser() {
+    private final UserService userService;
+
+    private AuthenticatedUser getAuthenticatedUser() {
         Jwt jwt = getJwt();
         return new AuthenticatedUser(
                 jwt.getSubject(),
@@ -24,6 +29,17 @@ public class AuthService {
                 jwt.getClaimAsString("give_name"),
                 jwt.getClaimAsString("family_name")
         );
+    }
+
+    public String getAuthenticatedUserEmail() {
+        AuthenticatedUser user = getAuthenticatedUser();
+        return user.email();
+    }
+
+    public User getCurrentUser() {
+        String authenticatedEmail = getAuthenticatedUserEmail();
+        return userService.findUserByEmail(authenticatedEmail);
+
     }
 
     private static Jwt getJwt() {
